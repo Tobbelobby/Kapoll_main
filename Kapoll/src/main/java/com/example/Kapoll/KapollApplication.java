823f69917016b65831b64402c71b4f537e7b28ff
Tobias Kristensen <tkr048@uib.no>
2022-10-21 10:57:47 +0200

    nå da

---
+++ Kapoll/src/main/java/com/example/Kapoll/KapollApplication.java
@@ -0,0 +1,13 @@
+package com.example.Kapoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KapollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KapollApplication.class, args);
+	}
+
+}
---
+++ Kapoll/src/test/java/com/example/Kapoll/KapollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Kapoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KapollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
