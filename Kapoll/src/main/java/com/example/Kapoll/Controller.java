e50984c7c2be590b38e021b7b5e6967bb4b4da78
idahe <xek010@uib.no>
2022-10-27 16:38:36 +0200

    added putMapping

--- Kapoll/src/main/java/com/example/Kapoll/Controller.java
+++ Kapoll/src/main/java/com/example/Kapoll/Controller.java
@@ -10,8 +10,7 @@ import com.example.Kapoll.Kapoll_db.tables.Poll;
 import com.example.Kapoll.Kapoll_db.tables.Poll_result;
 import com.example.Kapoll.Kapoll_db.tables.Voters;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,28 +22,73 @@ public class Controller {
     private PollDAO pollDAO = new PollDAO();
     private PollResDAO pollResDAO = new PollResDAO();
     private VoterDAO voterDAO = new VoterDAO();
+
     //public Controller(){}
 
+    //////////////////////// KAPOLLER
     @GetMapping("/Kapoller")
     List<Kapoller> GetAllKapollers(){
         return kapollerDAO.getAll();
     }
 
+    @GetMapping("/Kapoller/{id}")
+    Kapoller GetKapoller(@PathVariable Long id){ return kapollerDAO.get(id);}
+
+    //FIX! test
+    @PutMapping("Kapoller")
+    void newKapoller(@RequestBody Kapoller newKapoller){
+        kapollerDAO.addAndSave(newKapoller);
+    }
+
+
+
+    //////////////////////// POLL
     @GetMapping("/Poll")
     List<Poll> GetAllPolls(){
         return pollDAO.getAll();
     }
 
+    @GetMapping("/Poll/{id}")
+    Poll GetPoll(@PathVariable Long id){ return pollDAO.get(id);}
+
+    @PutMapping("Poll")
+    void newPoll(@RequestBody Poll newPoll){
+        pollDAO.addAndSave(newPoll);
+    }
+
+
+
+    //////////////////////// POLLRESULT
     @GetMapping("/PollResult")
     List<Poll_result> GetAllPollRes(){
         return pollResDAO.getAll();
     }
 
+    @GetMapping("/PollResult/{id}")
+    Poll_result GetPollRes(@PathVariable Long id){ return pollResDAO.get(id);}
+
+    @PutMapping("PollResult")
+    void newPollRes(@RequestBody Poll_result newPollRes){
+        pollResDAO.addAndSave(newPollRes);
+    }
+
+
+
+    //////////////////////// VOTERS
     @GetMapping("/Voters")
     List<Voters> GetAllVoters(){
         return voterDAO.getAll();
     }
 
+    @GetMapping("/Voters/{id}")
+    Voters GetVote(@PathVariable Long id){ return voterDAO.get(id);}
+
+    @PutMapping("Voters")
+    void newVote(@RequestBody Voters newVote){voterDAO.addAndSave(newVote);}
+
+
+    //FIX - add mappings
+    //PostMapping, PutMapping, DeleteMapping,
 
 
 
